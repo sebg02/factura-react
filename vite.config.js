ea7436d3a3bc4279ef4cc381cfccdf64db73ae2e
Sebastián <103606314+sebg02@users.noreply.github.com>
2024-06-03 22:06:34 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://elkin-Torres.github.io/Entregable3/",
+  base:"https://sebg02.github.io/factura-react/",
 })
