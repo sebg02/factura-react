564ef01572988c36bc5791644fb2c6d2d55ab97a
Sebastián <103606314+sebg02@users.noreply.github.com>
2024-06-03 22:47:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://sebg02.github.io/factura-react/",
+  base:"https://github.com/sebg02/factura-react",
 })
